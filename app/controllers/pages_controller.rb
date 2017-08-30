f59fbfdb918d3d678bce397dabc8e58a85a8dfd7
Nazmi <nazmifeeroz@hotmail.com>
2017-08-30 18:49:05 +0800

    pages count with destroy

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,44 @@
 class PagesController < ApplicationController
 
 	def home
-		c = Count.find(1)
-		c.num += 1
-		c.save
+		
+		c = Count.find_by(page: "Home")
+		
+		if c
+			c.num += 1
+			c.save
+		else
+			Count.create(page: "Home", num: 0)
+
+		end
+
+		if Count.find_by(page: "About") == nil
+		 Count.create(page: "About", num: 0)
+		end
+
+		if Count.find_by(page: "service") == nil
+			Count.create(page: "service", num: 0) 
+		end
+			
 
 	end
 
 	def about
-		c = Count.find(2)
+		c = Count.find_by(page: "About")
 		c.num += 1
 		c.save
 	
 	end
 
 	def service
-		c = Count.find(3)
+		c = Count.find_by(page: "service")
 		c.num += 1
 		c.save
+	end
+
+	def destroy
+		Count.destroy_all
+		redirect_to root_path
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
 
 	get 'about', to: 'pages#about'
 	get 'service', to: 'pages#service'
+	get 'destroy', to: 'pages#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
